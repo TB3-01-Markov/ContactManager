c6bf3e239013aac64d28a20c08b6610eefad076b
kilfour <kilfour@gmail.com>
2026-05-22 09:44:12 +0200

    folder fix

--- indexScript1.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.getElementById("contactaddbar-btn");
-const contactSection = document.getElementById("contactadd-section");
-
-toggleBtn.addEventListener("click", () => {
-    contactSection.classList.toggle("hidden");
-});
